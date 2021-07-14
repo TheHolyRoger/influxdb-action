078c91df3f33d6f6b42b8c12af3f6a189e5a3c84
TheHolyRoger <theholyroger@theholyroger.com>
2021-07-14 14:50:07 +0100

    Add support for v1

--- install-influxdb.sh
+++ install-influxdb.sh
@@ -9,8 +9,31 @@ then
 else
   export INFLUXDB_DOWNLOAD_PATH="https://dl.influxdata.com/influxdb/releases"
 fi
-wget -q $INFLUXDB_DOWNLOAD_PATH/influxdb2-$INFLUXDB_VERSION-linux-amd64.tar.gz
-tar xvfz influxdb2-$INFLUXDB_VERSION-linux-amd64.tar.gz
-sudo cp influxdb2-$INFLUXDB_VERSION-linux-amd64/{influx,influxd} /usr/local/bin/
-rm -r influxdb2-$INFLUXDB_VERSION-linux-amd64/
-cd -
+
+echo "Influx Installing."
+if [ "$INFLUXDB_VERSION" = "v1" ];
+then
+  wget -q $INFLUXDB_DOWNLOAD_PATH/influxdb_1.8.6_amd64.deb
+  sudo dpkg -i influxdb_1.8.6_amd64.deb
+  sudo rm -r influxdb_1.8.6_amd64.deb
+  cd -
+elif [[ $INFLUXDB_VERSION = 1* ]];
+then
+  wget -q $INFLUXDB_DOWNLOAD_PATH/influxdb-${INFLUXDB_VERSION}_linux_amd64.tar.gz
+  tar xvfz influxdb-${INFLUXDB_VERSION}_linux_amd64.tar.gz
+  sudo cp influxdb-${INFLUXDB_VERSION}-1/usr/bin/{influx,influxd} /usr/local/bin/
+  sudo mkdir -p /etc/influxdb
+  sudo cp influxdb-${INFLUXDB_VERSION}-1/etc/influxdb/influxdb.conf /etc/influxdb/
+  rm -r influxdb-${INFLUXDB_VERSION}-1/
+  cd -
+else
+  wget -q $INFLUXDB_DOWNLOAD_PATH/influxdb2-${INFLUXDB_VERSION}-linux-amd64.tar.gz
+  tar xvfz influxdb2-${INFLUXDB_VERSION}-linux-amd64.tar.gz
+  sudo cp influxdb2-${INFLUXDB_VERSION}-linux-amd64/{influx,influxd} /usr/local/bin/
+  rm -r influxdb2-${INFLUXDB_VERSION}-linux-amd64/
+  cd -
+fi
+
+echo "Influx Installed."
+
+exit 0
--- setup-influxdb.sh
+++ setup-influxdb.sh
@@ -1,11 +1,25 @@
+echo "Setting up Influx"
 
 if [ "$INFLUXDB_START" = "true" ]
 then
-    influxd --http-bind-address :8086 --reporting-disabled > /dev/null 2>&1 &
-    until curl -s http://localhost:8086/health; do sleep 1; done
-    influx setup --host http://localhost:8086 -f \
-        -o $INFLUXDB_ORG \
-        -u $INFLUXDB_USER \
-        -p $INFLUXDB_PASSWORD \
-        -b $INFLUXDB_BUCKET
+    if [ "$INFLUXDB_VERSION" = "v1" ] || [[ $INFLUXDB_VERSION = 1* ]];
+    then
+        sudo influxd &
+    else
+        influxd --http-bind-address :8086 --reporting-disabled > /dev/null 2>&1 &
+    fi
+    echo "Waiting for influx to be up."
+    FINAL_WAIT_TIME=0
+    until [ $NEXT_WAIT_TIME -eq 20 ] | curl -s http://localhost:8086/health; do sleep $(( NEXT_WAIT_TIME++ )); done
+    echo "Influx Running."
+    if [ "$INFLUXDB_VERSION" = "v1" ] || [[ $INFLUXDB_VERSION = 1* ]];
+    then
+        exit 0
+    else
+        sudo influx setup --host http://localhost:8086 -f \
+            -o $INFLUXDB_ORG \
+            -u $INFLUXDB_USER \
+            -p $INFLUXDB_PASSWORD \
+            -b $INFLUXDB_BUCKET
+    fi
 fi
